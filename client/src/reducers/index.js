3ea83eecdb1b87972cde12adcb7b5975aaca6ec3
Scrinb0y <alvarezmatiasagustin@hotmail.com>
2021-08-27 21:22:24 -0300

    search by name fixed when no name given

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -26,9 +26,15 @@ function reducer(state = initialState, action) {
                 countriesBackup: action.payload
             }
         case SEARCH_COUNTRIES:
+            var countriesFound;
+            if (action.pay !== undefined) {
+                countriesFound = action.payload
+            } else {
+                countriesFound = state.countriesBackup
+            }
             return {
                 ...state,
-                countries: action.payload
+                countries: countriesFound
             }
         case GET_COUNTRY_DETAILS:
             return {
